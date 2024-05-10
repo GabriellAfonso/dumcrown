fdac54450b30bd35ed636ba643b1aa896c9b9b27
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2024-05-10 10:51:07 -0300

    WrapperContainer almost done

--- dumcrown/dumcrown/static/dumcrown/game-V0.1.3/scenes/emails.js
+++ dumcrown/dumcrown/static/dumcrown/game-V0.1.3/scenes/emails.js
@@ -16,9 +16,10 @@ import { createAllCards } from '../cards/base.js';
 //TODO: BASEAR A LOGICA NO BAU DO MINECRAFT
 
 class WrapperContainer extends Phaser.GameObjects.Container {
-    constructor(scene, x, y, maskHeight) {
+    constructor(scene, x, y, maskHeight, scrollable = false) {
         super(scene, x, y);
         this.maskHeight = maskHeight
+        this.scrollable = scrollable
         this.width = 1;
         this.height = 1;
         this.items = [];
@@ -31,8 +32,8 @@ class WrapperContainer extends Phaser.GameObjects.Container {
         this.items.push(item);
     }
 
-    updateLayout(scale, initialY, xGap, yGap) {
-        const maxItemsPerRow = 4;
+    updateLayout(scale, initialY, xGap, yGap, itemsPerRow) {
+        const maxItemsPerRow = itemsPerRow;
         const totalItems = this.items.length;
         const numRows = Math.ceil(totalItems / maxItemsPerRow);
 
@@ -43,31 +44,55 @@ class WrapperContainer extends Phaser.GameObjects.Container {
 
         for (let i = 0; i < totalItems; i++) {
             const item = this.items[i];
-            item.width = item.width * scale
-            item.height = item.height * scale
+            const itemWidth = item.width * scale
+            const itemHeight = item.height * scale
 
-            xPos = (i % maxItemsPerRow) * (item.width + xGap) - (maxItemsPerRow - 1) * (item.width + xGap) / 2;
-            yPos = Math.floor(i / maxItemsPerRow) * (item.height + yGap);
-
+            xPos = (i % maxItemsPerRow) * (itemWidth + xGap) - (maxItemsPerRow - 1) * (itemWidth + xGap) / 2;
+            yPos = Math.floor(i / maxItemsPerRow) * (itemHeight + yGap);
 
             item.x = xPos;
-            item.y = yPos - this.displayOriginY + item.height / 2 + 10;
+            item.y = yPos - this.displayOriginY + itemHeight / 2 + 10;
             item.setVisible(true);
             item.setScale(scale);
-
         }
 
 
 
         this.createMask()
-        this.createScrollbar()
-        // this.containerDisplay()
-        this.setInteractive({ draggable: true, cursor: 'pointer' });
 
-        this.on('drag', this.onDrag);
-        this.on('pointerdown', this.checkClick);
-        console.log('terminou')
+        if (this.scrollable) {
+            this.createScrollbar()
+            this.setLimits()
+        }
+
+        this.setupEventListeners()
+        this.setInteractive({ draggable: true });
+        this.containerDisplay()
     }
+
+    setupEventListeners() {
+        this.on('pointerdown', this.checkClick);
+
+        if (this.scrollable) {
+            this.on('drag', this.drag);
+            this.scene.input.on('wheel', (pointer, gameObjects, deltaX, deltaY, deltaZ) => {
+                this.onMouseWheel(deltaY);
+            });
+        }
+    }
+
+    onMouseWheel(deltaY) {
+        const scrollSpeed = deltaY / 4;
+
+        this.containerPosition = this.getPercentagePosition();
+
+        this.y = Phaser.Math.Clamp(this.y - scrollSpeed, this.containerLowerLimit, this.containerUpperLimit);
+        this.setScrollThumbPosition(this.containerPosition)
+
+        console.log(this.containerPosition);
+    }
+
+
 
     checkClick(pointer) {
         this.items.forEach(item => {
@@ -80,29 +105,23 @@ class WrapperContainer extends Phaser.GameObjects.Container {
         }, 200)
     }
 
-    onDrag(pointer, dragX, dragY) {
+    drag(pointer, dragX, dragY) {
         if (this.input.dragStartY !== undefined) {
-            const dy = this.input.dragStartY - dragY;
+            const verticalDragDelta = this.input.dragStartY - dragY;
 
-            const upperLimit = this.displayOriginY + this.initialMaskY
-            const lowerLimit = (-this.height / 2) + this.maskHeight + this.initialMaskY
-            this.y = Phaser.Math.Clamp(this.y - dy, lowerLimit, upperLimit);
+            this.y = Phaser.Math.Clamp(this.y - verticalDragDelta, this.containerLowerLimit, this.containerUpperLimit);
 
-
-
-            // Calcula a posição normalizada do objeto dentro do contêiner
-            const normalizedPosition = (this.y - upperLimit) / (lowerLimit - upperLimit);
-
-            // Calcula a porcentagem multiplicando pela escala de 100
-            const positionPercentage = normalizedPosition * 100;
-
-            // Ajusta o valor para garantir que esteja dentro do intervalo [0, 100]
-            this.containerPosition = Phaser.Math.Clamp(positionPercentage, 0, 100);
-            console.log(this.containerPosition);
+            this.containerPosition = this.getPercentagePosition()
             this.setScrollThumbPosition(this.containerPosition)
-            // this.updateScrollThumb()
             this.input.dragStartY = dragY;
         }
+    }
+
+    getPercentagePosition() {
+        const normalizedPosition = (this.y - this.containerUpperLimit) / (this.containerLowerLimit - this.containerUpperLimit);
+
+        const positionPercentage = normalizedPosition * 100;
+        return Phaser.Math.Clamp(positionPercentage, 0, 100);
     }
 
     createMask() {
@@ -113,6 +132,15 @@ class WrapperContainer extends Phaser.GameObjects.Container {
         this.maskGraphics.fillRect(this.x - this.width / 2, this.initialMaskY, this.width, this.maskHeight);
         this.maskShape = this.maskGraphics.createGeometryMask();
         this.setMask(this.maskShape);
+
+
+    }
+    setLimits() {
+        this.containerUpperLimit = this.displayOriginY + this.initialMaskY;
+        this.containerLowerLimit = (-this.height / 2) + this.maskHeight + this.initialMaskY;
+
+        this.thumbUpperLimit = this.initialMaskY + this.scrollThumbHeight / 2;
+        this.thumbLowerLimit = this.initialMaskY + this.maskHeight - this.scrollThumbHeight / 2;
     }
 
     containerDisplay() {
@@ -123,11 +151,12 @@ class WrapperContainer extends Phaser.GameObjects.Container {
         this.containerRect.setOrigin(0.5);
         this.add(this.containerRect);
     }
+
     createScrollbar() {
         const scrollbarWidth = 10; // largura da barra de rolagem
         this.scrollThumbHeight = 40; // altura igual à altura da máscara
         this.scrollRange = this.maskHeight - this.scrollThumbHeight;
-        const scrollbarColor = 0x666666; // cor d console.log('Y:', lowerLimit)a barra de rolagem
+        const scrollbarColor = 0x666666; // cor d console.log('Y:', this.thumbLowerLimit)a barra de rolagem
 
         this.scrollBar = this.scene.add.rectangle(
             this.x + this.width / 2 + scrollbarWidth / 2, // posição à direita do contêiner
@@ -154,14 +183,12 @@ class WrapperContainer extends Phaser.GameObjects.Container {
 
 
             if (this.scrollThumb.dragStartY !== undefined) {
-                const dy = dragY;
-                const upperLimit = this.initialMaskY + this.scrollThumbHeight / 2;
-                const lowerLimit = this.initialMaskY + this.maskHeight - this.scrollThumbHeight / 2;
-                this.scrollThumb.y = Phaser.Math.Clamp(dy, upperLimit, lowerLimit);
+                const verticalDragDelta = dragY;
+                this.scrollThumb.y = Phaser.Math.Clamp(verticalDragDelta, this.thumbUpperLimit, this.thumbLowerLimit);
 
                 // Calcula a posição normalizada da scrollThumb dentro do scrollRange
 
-                const normalizedPosition = (this.scrollThumb.y - upperLimit) / this.scrollRange;
+                const normalizedPosition = (this.scrollThumb.y - this.thumbUpperLimit) / this.scrollRange;
 
                 // Calcula a porcentagem multiplicando pela escala de 100
                 const thumbPositionPercentage = normalizedPosition * 100;
@@ -169,7 +196,7 @@ class WrapperContainer extends Phaser.GameObjects.Container {
                 // Ajusta o valor para garantir que esteja dentro do intervalo [0, 100]
                 this.scrollThumbPosition = Phaser.Math.Clamp(thumbPositionPercentage, 0, 100);
 
-                this.moveContent(); // Move o conteúdo conforme a posição da scrollThumb
+                this.moveContent();
                 console.log(this.scrollThumbPosition);
 
                 this.scrollThumb.dragStartY = dragY;
@@ -180,24 +207,19 @@ class WrapperContainer extends Phaser.GameObjects.Container {
         });
     }
     moveContent() {
-        const scrollableHeight = this.height - this.maskHeight; // A quantidade de conteúdo que pode ser rolada
-        const contentOffset = this.scrollThumbPosition * (scrollableHeight / 100); // O deslocamento do conteúdo baseado na porcentagem da scrollThumb
+        const scrollableHeight = this.height - this.maskHeight;
+        const contentOffset = this.scrollThumbPosition * (scrollableHeight / 100);
         this.y = this.displayOriginY + this.initialMaskY - contentOffset;
     }
 
     setScrollThumbPosition(percentage) {
-        // Converte a porcentagem em uma posição normalizada
-        const upperLimit = this.initialMaskY + this.scrollThumbHeight / 2;
-        const lowerLimit = this.initialMaskY + this.maskHeight - this.scrollThumbHeight / 2;
+
         const normalizedPosition = Phaser.Math.Clamp(percentage, 0, 100) / 100;
 
-        // Calcula a posição real do scrollThumb.y dentro dos limites superior e inferior
-        const thumbY = upperLimit + normalizedPosition * this.scrollRange;
+        const thumbY = this.thumbUpperLimit + normalizedPosition * this.scrollRange;
 
-        // Define a posição do scrollThumb.y garantindo que esteja dentro dos limites superior e inferior
-        this.scrollThumb.y = Phaser.Math.Clamp(thumbY, upperLimit, lowerLimit);
+        this.scrollThumb.y = Phaser.Math.Clamp(thumbY, this.thumbUpperLimit, this.thumbLowerLimit);
 
-        // Atualiza a posição normalizada do scrollThumb
         this.scrollThumbPosition = percentage;
     }
 }
@@ -238,7 +260,7 @@ export class EmailsScene extends Phaser.Scene {
 
 
 
-        this.container = new WrapperContainer(this, 954, centerY, 670)
+        this.container = new WrapperContainer(this, 954, centerY, 670, true)
         this.cards = createAllCards(this, true)
 
         // this.cards[10].setVisible(true)
@@ -248,7 +270,7 @@ export class EmailsScene extends Phaser.Scene {
 
             }
         }
-        this.container.updateLayout(0.55, 80, 80, 60);
+        this.container.updateLayout(0.55, 80, 80, 60, 4);
     }
 
     update() {
